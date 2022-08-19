fd2529e4f5b4f3a7e6f5cf8fd1a296e0f25632fa
Maria Kagermanova <maria.kgm04@gmail.com>
2022-08-19 23:21:09 +0300

    feat: add start game button

--- src/pages/Audiocall/Audiocall.tsx
+++ src/pages/Audiocall/Audiocall.tsx
@@ -1,11 +1,18 @@
-import { FC } from 'react';
-import ContentWrapper from '../../layouts/ContentWrapper';
+import { FC, useState } from 'react';
 import s from './Audiocall.module.scss';
+import ContentWrapper from '../../layouts/ContentWrapper';
+// import { useGetWordsQuery } from '../../services/wordsApi';
 
 const Audiocall: FC = () => {
+  const [gameStarted, setGameStart] = useState(false);
+  // const { data, error, isLoading } = useGetWordsQuery({ group: 0, page: 0 });
+
+  console.log(gameStarted);
   return (
     <ContentWrapper className={s.audiocallWrapper}>
-      <div>chip</div>
+      <button type="button" onClick={(): void => setGameStart(!gameStarted)}>
+        {!gameStarted ? 'Start Game' : 'End Game'}
+      </button>
     </ContentWrapper>
   );
 };
