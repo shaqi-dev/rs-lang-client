70cf6da114c659a99e69d1184bff69902a28f8d8
Maria Kagermanova <maria.kgm04@gmail.com>
2022-08-19 23:52:13 +0300

    feat: create component for audiocall-game words

--- src/pages/Audiocall/Audiocall.tsx
+++ src/pages/Audiocall/Audiocall.tsx
@@ -1,18 +1,25 @@
 import { FC, useState } from 'react';
 import s from './Audiocall.module.scss';
 import ContentWrapper from '../../layouts/ContentWrapper';
+import AudiocallGame from '../../components/AudiocallGame';
 // import { useGetWordsQuery } from '../../services/wordsApi';
 
 const Audiocall: FC = () => {
   const [gameStarted, setGameStart] = useState(false);
   // const { data, error, isLoading } = useGetWordsQuery({ group: 0, page: 0 });
 
-  console.log(gameStarted);
   return (
     <ContentWrapper className={s.audiocallWrapper}>
-      <button type="button" onClick={(): void => setGameStart(!gameStarted)}>
+      {/* <button type="button" onClick={(): void => setGameStart(!gameStarted)}>
         {!gameStarted ? 'Start Game' : 'End Game'}
-      </button>
+      </button> */}
+      {!gameStarted ? (
+        <button type="button" onClick={(): void => setGameStart(!gameStarted)}>
+          Start Game
+        </button>
+      ) : (
+        <AudiocallGame />
+      )}
     </ContentWrapper>
   );
 };
