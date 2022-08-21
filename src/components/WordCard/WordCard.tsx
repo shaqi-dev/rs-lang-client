57c11710ac4a7a354536ad68e4fbed7eea6bdfa8
Vladislav Zubakin <shaqimusic@gmail.com>
2022-08-21 03:30:27 +0300

    feat: add audio button

--- src/components/WordCard/WordCard.tsx
+++ src/components/WordCard/WordCard.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import { FC } from 'react';
 import { Word } from '../../interfaces/words';
 import { API_BASE } from '../../services/endpoints';
+import { ReactComponent as PlayIcon } from '../../assets/svg/play-sound-icon.svg';
 import s from './WordCard.module.scss';
 
 export interface WordCardProps {
@@ -16,10 +19,23 @@ const WordCard: FC<WordCardProps> = ({ word }) => {
     textMeaningTranslate,
     textExample,
     textExampleTranslate,
+    audio: audioSrc,
+    audioExample: audioExampleSrc,
+    audioMeaning: audioMeaningSrc,
     image,
   } = word;
 
   const imageSource = `${API_BASE}/${image}`;
+
+  const playAudio = async (): Promise<void> => {
+    const audio = new Audio(`${API_BASE}/${audioSrc}`);
+    const audioExample = new Audio(`${API_BASE}/${audioExampleSrc}`);
+    const audioMeaning = new Audio(`${API_BASE}/${audioMeaningSrc}`);
+
+    audio.addEventListener('ended', () => audioMeaning.play());
+    audioMeaning.addEventListener('ended', () => audioExample.play());
+    audio.play();
+  };
 
   return (
     <div className={s.root}>
@@ -30,6 +46,7 @@ const WordCard: FC<WordCardProps> = ({ word }) => {
         <div className={s.word}>
           <span className={s.word_original}>{wordOriginal} </span>
           <span className={s.word_transcription}>{transcription}</span>
+          <PlayIcon className={s.word_playButton} onClick={playAudio} />
           <p className={s.word_translate}>{wordTranslate}</p>
         </div>
         <div className={s.textBlock}>
