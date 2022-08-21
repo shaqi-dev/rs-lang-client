b72ff949c527f0dc26851d985b55702ca118e125
Vladislav Zubakin <shaqimusic@gmail.com>
2022-08-21 16:34:31 +0300

    feat: add view buttons

--- src/pages/Textbook/Textbook.tsx
+++ src/pages/Textbook/Textbook.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ContentWrapper from '../../layouts/ContentWrapper';
 import WordsGroupList from '../../components/WordsGroupList';
@@ -21,6 +21,7 @@ import Button from '../../components/Button';
 const Textbook: FC = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const [view, setView] = useState<'textbook' | 'vocabulary'>('textbook');
   const group: number = useAppSelector(selectCurrentGroup);
   const page: number = useAppSelector(selectCurrentPage);
   const word: Word | null = useAppSelector(selectCurrentWord);
@@ -41,6 +42,26 @@ const Textbook: FC = () => {
 
   return (
     <ContentWrapper className={s.wrapper}>
+      <section className={s.viewSection}>
+        <Button
+          type="button"
+          buttonStyle="link"
+          inactive={view === 'vocabulary'}
+          className={s.viewSection_button}
+          onClick={(): void => setView('textbook')}
+        >
+          Учебник
+        </Button>
+        <Button
+          type="button"
+          buttonStyle="link"
+          inactive={view === 'textbook'}
+          className={s.viewSection_button}
+          onClick={(): void => setView('vocabulary')}
+        >
+          Словарь
+        </Button>
+      </section>
       <section className={s.groupsSection}>
         <p className={s.sectionTitle}>Уровень сложности</p>
         <WordsGroupList onClickItem={handleClickWordsGroupItem} />
