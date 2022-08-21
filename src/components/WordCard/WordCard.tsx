0ef2319e3e46b2eb9ca22bdead1f5b40fe75bc2e
Vladislav Zubakin <shaqimusic@gmail.com>
2022-08-21 04:53:19 +0300

    refactor: extract audio into hook

--- src/components/WordCard/WordCard.tsx
+++ src/components/WordCard/WordCard.tsx
@@ -1,17 +1,15 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import { Word } from '../../interfaces/words';
 import { API_BASE } from '../../services/endpoints';
 import { ReactComponent as PlayIcon } from '../../assets/svg/play-sound-icon.svg';
 import s from './WordCard.module.scss';
+import useAudio from '../../hooks/useAudio';
 
 export interface WordCardProps {
   word: Word;
 }
 
 const WordCard: FC<WordCardProps> = ({ word }) => {
-  const [isPlaying, setIsPlaying] = useState<boolean>(false);
   const {
     word: wordOriginal,
     wordTranslate,
@@ -20,28 +18,12 @@ const WordCard: FC<WordCardProps> = ({ word }) => {
     textMeaningTranslate,
     textExample,
     textExampleTranslate,
-    audio: audioSrc,
-    audioExample: audioExampleSrc,
-    audioMeaning: audioMeaningSrc,
     image,
   } = word;
 
   const imageSource = `${API_BASE}/${image}`;
 
-  const audio = new Audio(`${API_BASE}/${audioSrc}`);
-  const audioMeaning = new Audio(`${API_BASE}/${audioMeaningSrc}`);
-  const audioExample = new Audio(`${API_BASE}/${audioExampleSrc}`);
-
-  audio.addEventListener('ended', () => audioMeaning.play());
-  audioMeaning.addEventListener('ended', () => audioExample.play());
-  audioExample.addEventListener('ended', () => setIsPlaying(false));
-
-  const playAudio = (): void => {
-    if (!isPlaying) {
-      setIsPlaying(true);
-      audio.play();
-    }
-  };
+  const [play] = useAudio(word);
 
   return (
     <div className={s.root}>
@@ -52,7 +34,7 @@ const WordCard: FC<WordCardProps> = ({ word }) => {
         <div className={s.word}>
           <span className={s.word_original}>{wordOriginal} </span>
           <span className={s.word_transcription}>{transcription}</span>
-          <PlayIcon className={s.word_playButton} onClick={playAudio} />
+          <PlayIcon className={s.word_playButton} onClick={play} />
           <p className={s.word_translate}>{wordTranslate}</p>
         </div>
         <div className={s.textBlock}>
