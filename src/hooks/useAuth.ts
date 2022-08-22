b07884216aa34ce378ee7037f03ecf1bf9ba7460
Vladislav Zubakin <shaqimusic@gmail.com>
2022-08-23 02:12:15 +0300

    fix: reauth token parsing

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -23,6 +23,19 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
+const refreshQuery = fetchBaseQuery({
+  baseUrl: API_BASE,
+  prepareHeaders: (headers, { getState }) => {
+    const { refreshToken } = (getState() as RootState).auth;
+
+    if (refreshToken) {
+      headers.set('Authorization', `Bearer ${refreshToken}`);
+    }
+
+    return headers;
+  },
+});
+
 const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (
   args,
   api,
@@ -30,11 +43,12 @@ const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQue
 ) => {
   let result = await baseQuery(args, api, extraOptions);
 
-  const invalidToken = result.error && result.error.status === 401;
+  const invalidToken =
+    result.error && result.error.status === 'PARSING_ERROR' && result.error.originalStatus === 401;
 
   if (invalidToken) {
     const { username, userId } = (api.getState() as RootState).auth;
-    const { data } = await baseQuery(`users/${userId}/tokens`, api, extraOptions);
+    const { data } = await refreshQuery(`users/${userId}/tokens`, api, extraOptions);
 
     if (data) {
       const { token: accessToken, refreshToken } = data as SignInResponse;
