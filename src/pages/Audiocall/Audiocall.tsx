c2ef40139f12b96400db5115f9338b833b813bfc
Maria Kagermanova <maria.kgm04@gmail.com>
2022-08-23 15:28:16 +0300

    feat: add groups choise to audiocall words

--- src/pages/Audiocall/Audiocall.tsx
+++ src/pages/Audiocall/Audiocall.tsx
@@ -2,18 +2,29 @@ import { FC, useState } from 'react';
 import s from './Audiocall.module.scss';
 import ContentWrapper from '../../layouts/ContentWrapper';
 import AudiocallGame from '../../components/AudiocallGame';
+import WordsGroupList from '../../components/WordsGroupList';
 
 const Audiocall: FC = () => {
   const [gameStarted, setGameStart] = useState(false);
+  const [selectedGroup, setGroup] = useState<number>(0);
+  const [pageNumber, setPageNumber] = useState<number>(0);
+
+  const handleClickWordsGroupItem = (groupName: string): void => {
+    setGroup(Number(groupName[1]));
+    setPageNumber(Math.floor(Math.random() * 30));
+  };
 
   return (
     <ContentWrapper className={s.audiocallWrapper}>
       {!gameStarted ? (
-        <button type="button" onClick={(): void => setGameStart(!gameStarted)}>
-          Start Game
-        </button>
+        <>
+          <WordsGroupList onClickItem={handleClickWordsGroupItem} />
+          <button type="button" onClick={(): void => setGameStart(!gameStarted)}>
+            Start Game
+          </button>
+        </>
       ) : (
-        <AudiocallGame />
+        <AudiocallGame selectedGroup={selectedGroup} pageNumber={pageNumber} />
       )}
     </ContentWrapper>
   );
