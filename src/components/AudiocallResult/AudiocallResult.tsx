e914f1e9c4a6b41bbc1eadd46a89aad6e8ef7aec
Maria Kagermanova <maria.kgm04@gmail.com>
2022-08-24 16:27:15 +0300

    feat: add audio to result words

--- src/components/AudiocallResult/AudiocallResult.tsx
+++ src/components/AudiocallResult/AudiocallResult.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { useAppSelector } from '../../hooks/redux';
 import { Word } from '../../interfaces/words';
+import { API_BASE } from '../../services/endpoints';
 import {
   selectAudiocallWrongAnswers,
   selectAudiocallCorrectAnswers,
@@ -9,6 +10,13 @@ import {
 const AudiocallResult: FC = () => {
   const audiocallWrongAnswers = useAppSelector(selectAudiocallWrongAnswers);
   const audiocallCorrectAnswers = useAppSelector(selectAudiocallCorrectAnswers);
+
+  const playAudio = (audioLink: string): void => {
+    const audioSource = `${API_BASE}/${audioLink}`;
+    const audio = new Audio(audioSource);
+    audio.play();
+  };
+
   return (
     <div>
       <p>{`Correct answers (${audiocallCorrectAnswers.length}):`}</p>
@@ -16,6 +24,9 @@ const AudiocallResult: FC = () => {
         {audiocallCorrectAnswers.map((answer: Word) => {
           return (
             <li key={answer.word}>
+              <button type="button" onClick={(): void => playAudio(answer.audio)}>
+                Play Audio
+              </button>
               <p>
                 {answer.word} - {answer.wordTranslate}
               </p>
@@ -28,6 +39,9 @@ const AudiocallResult: FC = () => {
         {audiocallWrongAnswers.map((answer) => {
           return (
             <li key={answer.word}>
+              <button type="button" onClick={(): void => playAudio(answer.audio)}>
+                Play Audio
+              </button>
               <p>
                 {answer.word} - {answer.wordTranslate}
               </p>
