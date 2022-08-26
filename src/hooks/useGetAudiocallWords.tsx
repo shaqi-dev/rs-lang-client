918d0be67c01e2bb36abad43b351e45c55c75a8c
Maria Kagermanova <maria.kgm04@gmail.com>
2022-08-26 22:09:45 +0300

    feat: change fetch method

---
+++ src/hooks/useGetAudiocallWords.tsx
@@ -0,0 +1,28 @@
+import { SerializedError } from '@reduxjs/toolkit';
+import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query';
+import { useEffect } from 'react';
+import { Word } from '../interfaces/words';
+import { useLazyGetWordsQuery } from '../services/wordsApi';
+
+const useGetAudiocallWords = (
+  group: number,
+  page: number,
+): {
+  data: Word[] | undefined;
+  error: FetchBaseQueryError | SerializedError | undefined;
+  isLoading: boolean;
+} => {
+  const [fetchGuestWords, guestWordsResponse] = useLazyGetWordsQuery();
+
+  useEffect(() => {
+    fetchGuestWords({ group, page });
+  }, [fetchGuestWords, group, page]);
+
+  return {
+    data: guestWordsResponse.data,
+    error: guestWordsResponse.error,
+    isLoading: guestWordsResponse.isLoading,
+  };
+};
+
+export default useGetAudiocallWords;
