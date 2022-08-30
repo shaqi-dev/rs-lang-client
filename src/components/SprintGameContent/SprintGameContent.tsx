0f764bcccef2d7018c2ff99bbf3583efbd3de4e4
Mikeil Akovity (1iekim) <mikeil98@mail.ru>
2022-08-30 21:34:50 +0300

    feat: add sprint game content box

---
+++ src/components/SprintGameContent/SprintGameContent.tsx
@@ -0,0 +1,61 @@
+import { FC } from 'react';
+import ContentWrapper from '../../layouts/ContentWrapper';
+import Button from '../Button';
+import s from './SprintGame.module.scss';
+
+interface SprintGameProps {
+  seconds: number;
+  answerCount: number;
+  word?: string;
+  translate?: string;
+  disabledBtn: boolean;
+  checkWord: (answer: boolean) => void;
+  isLoading: boolean;
+}
+
+const SprintGameContent: FC<SprintGameProps> = ({
+  seconds,
+  answerCount,
+  word,
+  translate,
+  disabledBtn,
+  checkWord,
+  isLoading,
+}) => {
+  if (isLoading) return <p>Loading....</p>;
+  return (
+    <ContentWrapper>
+      <section>
+        <div className={s.content}>
+          <span className={s.content_timer}>{seconds}</span>
+          <span className={s.content_counter}>Подряд: {answerCount}</span>
+        </div>
+        <div className={s.wordContainer}>
+          <h2>{word || 'Слова закончились!'}</h2>
+          <span>=</span>
+          <h2>{translate || 'Перевод'}?</h2>
+        </div>
+      </section>
+      <section className={s.button}>
+        <Button
+          className={s.button_true}
+          disabled={disabledBtn}
+          type="button"
+          onClick={(): void => checkWord(true)}
+        >
+          Верно
+        </Button>
+        <Button
+          className={s.button_false}
+          disabled={disabledBtn}
+          type="button"
+          onClick={(): void => checkWord(false)}
+        >
+          Неверно
+        </Button>
+      </section>
+    </ContentWrapper>
+  );
+};
+
+export default SprintGameContent;
