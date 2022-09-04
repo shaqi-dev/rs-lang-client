835775f71b39ed2a2d1b8c342dc44dbcc04f592e
Mikeil Akovity (1iekim) <mikeil98@mail.ru>
2022-09-04 22:56:52 +0300

    feat: game process save to stats

--- src/components/SprintGameContent/SprintGameContent.tsx
+++ src/components/SprintGameContent/SprintGameContent.tsx
@@ -17,9 +17,9 @@ interface SprintGameProps {
   answerCount: number;
   word?: string;
   translate?: string;
-  disabledBtn: boolean;
+  // disabledBtn: boolean;
   checkWord: (answer: boolean) => void;
-  isLoading: boolean;
+  // isLoading: boolean;
 }
 
 const SprintGameContent: FC<SprintGameProps> = ({
@@ -27,9 +27,9 @@ const SprintGameContent: FC<SprintGameProps> = ({
   answerCount,
   word,
   translate,
-  disabledBtn,
+  // disabledBtn,
   checkWord,
-  isLoading,
+  // isLoading,
 }) => {
   const dispatch = useAppDispatch();
 
@@ -115,43 +115,47 @@ const SprintGameContent: FC<SprintGameProps> = ({
     };
   }, [checkWord, word]);
 
-  if (isLoading) return <p>Loading....</p>;
+  // if (isLoading) return <p>Loading....</p>;
 
-  return (
-    <ContentWrapper>
-      <section>
-        <div className={s.content}>
-          <span className={s.content_timer}>{seconds}</span>
-          <span className={s.content_counter}>Подряд: {answerCount}</span>
-        </div>
-        <div className={s.wordContainer}>
-          <h2>{word || 'Слова закончились!'}</h2>
-          {word && <span>=</span>}
-          {word && <h2>{translate || 'Перевод'}?</h2>}
-        </div>
-      </section>
-      {word && (
-        <section className={s.button}>
-          <Button
-            className={s.button_true}
-            disabled={disabledBtn}
-            type="button"
-            onClick={(): void => checkWord(true)}
-          >
-            Верно
-          </Button>
-          <Button
-            className={s.button_false}
-            disabled={disabledBtn}
-            type="button"
-            onClick={(): void => checkWord(false)}
-          >
-            Неверно
-          </Button>
+  if (word) {
+    return (
+      <ContentWrapper>
+        <section>
+          <div className={s.content}>
+            <span className={s.content_timer}>{seconds}</span>
+            <span className={s.content_counter}>Подряд: {answerCount}</span>
+          </div>
+          <div className={s.wordContainer}>
+            <h2>{word || 'Слова закончились!'}</h2>
+            {word && <span>=</span>}
+            {word && <h2>{translate || 'Перевод'}?</h2>}
+          </div>
         </section>
-      )}
-    </ContentWrapper>
-  );
+        {word && (
+          <section className={s.button}>
+            <Button
+              className={s.button_true}
+              // disabled={disabledBtn}
+              type="button"
+              onClick={(): void => checkWord(true)}
+            >
+              Верно
+            </Button>
+            <Button
+              className={s.button_false}
+              // disabled={disabledBtn}
+              type="button"
+              onClick={(): void => checkWord(false)}
+            >
+              Неверно
+            </Button>
+          </section>
+        )}
+      </ContentWrapper>
+    );
+  }
+
+  return null;
 };
 
 export default SprintGameContent;
