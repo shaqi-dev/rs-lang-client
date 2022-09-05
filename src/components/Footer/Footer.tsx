2242fe977bf29c4b2a5a1f9c68321f16c05d029b
Vladislav Zubakin <shaqimusic@gmail.com>
2022-09-05 22:07:51 +0300

    feat: improve footer & global styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { useLocation } from 'react-router-dom';
 import ContentWrapper from '../../layouts/ContentWrapper';
+import { ReactComponent as RsSchoolLogo } from '../../assets/svg/rs-school-logo.svg';
 import s from './Footer.module.scss';
 
 const Footer: FC = () => {
@@ -9,7 +10,24 @@ const Footer: FC = () => {
   if (!location.pathname.split('/').includes('games')) {
     return (
       <footer className={s.root}>
-        <ContentWrapper>Footer</ContentWrapper>
+        <ContentWrapper className={s.wrapper}>
+          <span className={s.year}>2022</span>
+          <ul className={s.developersList}>
+            <span className={s.developersTitle}>Разработчики:</span>
+            <li className={s.developersItem}>
+              <a href="https://github.com/maria98kgm">maria98kgm</a>
+            </li>
+            <li className={s.developersItem}>
+              <a href="https://github.com/1iekim">1iekim</a>
+            </li>
+            <li className={s.developersItem}>
+              <a href="https://github.com/shaqi-dev">shaqi-dev</a>
+            </li>
+          </ul>
+          <a href="https://rs.school/js/">
+            <RsSchoolLogo className={s.rslogo} />
+          </a>
+        </ContentWrapper>
       </footer>
     );
   }
