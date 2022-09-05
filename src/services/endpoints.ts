7433173246ca54cc7a7d19b12a8fcf8d7d402de1
Vladislav Zubakin <shaqimusic@gmail.com>
2022-09-05 19:03:59 +0300

    feat: remove unused code

--- src/services/endpoints.ts
+++ src/services/endpoints.ts
@@ -1,7 +1,7 @@
 export const API_BASE = 'https://rslang-team-17-shaqi-dev.herokuapp.com';
 
-export const API_SIGNIN = `${API_BASE}/signin`;
+// export const API_SIGNIN = `${API_BASE}/signin`;
 
-export const API_USERS = `${API_BASE}/users`;
+// export const API_USERS = `${API_BASE}/users`;
 
-export const API_WORDS = `${API_BASE}/words`;
+// export const API_WORDS = `${API_BASE}/words`;
