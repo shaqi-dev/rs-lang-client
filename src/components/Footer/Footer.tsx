e4ee213c2f168c3c1fc2a1d8f877a75076b19cdc
Vladislav Zubakin <shaqimusic@gmail.com>
2022-09-05 21:34:04 +0300

    feat: hide footer on game pages

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,20 @@
 import { FC } from 'react';
+import { useLocation } from 'react-router-dom';
 import ContentWrapper from '../../layouts/ContentWrapper';
 import s from './Footer.module.scss';
 
 const Footer: FC = () => {
-  return (
-    <footer className={s.root}>
-      <ContentWrapper>Footer</ContentWrapper>
-    </footer>
-  );
+  const location = useLocation();
+
+  if (!location.pathname.split('/').includes('games')) {
+    return (
+      <footer className={s.root}>
+        <ContentWrapper>Footer</ContentWrapper>
+      </footer>
+    );
+  }
+
+  return null;
 };
 
 export default Footer;
